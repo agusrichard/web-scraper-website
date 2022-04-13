cf100b4013223805794c9cf6522a646a15e1452e
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-13 21:16:28 +0700

    Create celery worker

---
+++ worker/worker.py
@@ -0,0 +1,9 @@
+from time import time
+from celery import Celery
+
+worker = Celery(__name__, broker='redis://localhost:6379/0')
+
+@worker.task(name='create_task')
+def create_task(task_name):
+    time.sleep(1)
+    return 'Hello World'
