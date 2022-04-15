d51306b9ce6887cf261717b9cecc29737d83f3bd
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-16 00:31:21 +0700

    Add django migration

--- website/models.py
+++ website/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+SCRAPPING_STATUSES = (
+    (0, "Pending"),
+    (1, "In Progress"),
+    (2, "Completed"),
+    (3, "Failed"),
+)
+
+
+class ScrappingHistory(models.Model):
+    start_datetime = models.DateTimeField(auto_now_add=True)
+    end_datetime = models.DateTimeField(null=True, blank=True)
+    status = models.IntegerField(choices=SCRAPPING_STATUSES, default=0)
+
+    def __str__(self):
+        return str(self.status)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=500)
+    subtitle = models.TextField()
+    author = models.CharField(max_length=100)
+    image_src = models.TextField()
+    url = models.TextField()
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    scrapping_history = models.ForeignKey(ScrappingHistory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.title)
