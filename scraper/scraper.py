4ddaffbfe32ddd1e615b74dc5be2992fa1946578
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-17 03:55:00 +0700

    Working on scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,14 +1,21 @@
 import os
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-BASE_URL = 'https://www.traveloka.com/en-id/'
+BASE_URL = "https://medium.com/search?q=programming"
+
 
 class Scraper(webdriver.Chrome):
     def __init__(self, driver_path=None, teardown=False, implicitly_time_to_wait=10):
         base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        driver_dir = os.path.join(base_path, 'driver')
+        driver_dir = os.path.join(base_path, "driver")
 
-        self.driver_path = driver_path if driver_path is not None else os.path.join(driver_dir, 'chromedriver.exe') 
+        self.driver_path = (
+            driver_path
+            if driver_path is not None
+            else os.path.join(driver_dir, "chromedriver.exe")
+        )
         self.teardown = teardown
         self.implicitly_time_to_wait = implicitly_time_to_wait
 
@@ -16,32 +23,47 @@ class Scraper(webdriver.Chrome):
 
         self.implicitly_wait(self.implicitly_time_to_wait)
         self.maximize_window()
+        self.result = []
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, *args):
         if self.teardown:
             self.quit()
 
     def go_to_first_page(self):
         self.get(BASE_URL)
 
-    def fill_in_hotel_destination_input(self, destination):
-        input_element = self.find_element_by_css_selector(
-            f'input[placeholder="City, hotel, place to go"]'
-        )
-        input_element.send_keys(destination)
+    def get_article_section(self):
+        css_selector = "article"
+        sections = self.find_elements(by=By.CSS_SELECTOR, value=css_selector)
+        for section in sections:
+            print(str(section))
 
-    def click_nth_element_of_search_results(self, nth_index=0):
-        try:
-            search_result_elements = self.find_elements_by_css_selector(
-                f'div[data-testid="dropdown-menu-item"]'
-            )
-            selected_element = search_result_elements[nth_index]
-            selected_element.click()
-        except IndexError:
-            print('Index error')
+    def scroll_down(self, sleep_time=3, num_scrolls=10):
+        # Get scroll height.
+        last_height = self.execute_script("return document.body.scrollHeight")
 
-if __name__ == '__main__':
+        for _ in range(num_scrolls):
+            self.get_article_section()
+
+            # Scroll down to the bottom.
+            self.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load the page.
+            time.sleep(sleep_time)
+            self.find_element(
+                by=By.XPATH, value="//*[contains(text(), 'Show more')]"
+            ).click()
+
+            # Calculate new scroll height and compare with last scroll height.
+            new_height = self.execute_script("return document.body.scrollHeight")
+
+            if new_height == last_height:
+                break
+
+            last_height = new_height
+
+
+if __name__ == "__main__":
     with Scraper() as scraper:
         scraper.go_to_first_page()
-        scraper.fill_in_hotel_destination_input('Jakarta')
-        scraper.click_nth_element_of_search_results(0)
+        scraper.scroll_down()
