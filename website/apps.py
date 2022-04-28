6143f92ef7091a2d25e7a7b2de1d36cbfada9431
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-28 23:03:36 +0700

    Run background scheduler to scrape

--- website/apps.py
+++ website/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class WebsiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "website"
+
+    def ready(self):
+        from .scheduler import start
+
+        start()
