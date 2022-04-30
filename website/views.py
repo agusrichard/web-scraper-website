ed2b86f8e77a38cc4f99fd14770a3bb61e0a2c3d
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-30 20:27:45 +0700

    Add pagination on home page

--- website/views.py
+++ website/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .tasks import scrape_website
-from .models import ScrapingHistory
+from .models import ScrapingHistory, Article
 
 
 def home(request):
-    return render(request, "website/home.html", {"page_title": "Home"})
+    data = Article.objects.all().order_by("-created_date")
+    page = request.GET.get("page", 1)
+
+    paginator = Paginator(data, 20)
+    page_range = paginator.get_elided_page_range(number=page)
+    try:
+        page_list = paginator.page(page)
+    except PageNotAnInteger:
+        page_list = paginator.page(1)
+    except EmptyPage:
+        page_list = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        "website/home.html",
+        {"page_title": "Home", "articles": page_list, "page_range": page_range},
+    )
 
 
 def scraping_history(request):
-    histories = ScrapingHistory.objects.all()
+    histories = ScrapingHistory.objects.all().order_by("-start_datetime")
 
     return render(
         request,
